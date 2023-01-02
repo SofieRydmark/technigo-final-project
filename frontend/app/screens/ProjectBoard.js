472280037f6b5fb5b3d45d865ff2a47944c57257
SofieRydmark <you@example.com>
2023-01-02 14:50:16 +0100

    update projectboard

--- frontend/app/screens/ProjectBoard.js
+++ frontend/app/screens/ProjectBoard.js
@@ -1,20 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { View, ScrollView, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 import colors from '../config/colors'
+import user from '../reducers/user'
 
 const ProjectBoard = ({ navigation }) => {
+  const accessToken = useSelector((store) => store.user.accessToken)
+  const email = useSelector((store) => store.user.email)
+  const dispatch = useDispatch()
+
+  const logout = () => {
+    console.log('logged out')
+    dispatch(user.actions.setEmail(null))
+    dispatch(user.actions.setAccessToken(null))
+  }
+
+  console.log('accesstoken null', accessToken)
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigation.navigate('SignIn')
+    }
+  }, [accessToken])
+
   return (
     <ScrollView contentContainerStyle={styles.background}>
-      <View style={styles.header}>
-        <Text style={styles.headerH1}>Projectboard</Text>
-      </View>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('SingleProject')}
-        style=''
-        title='Project X'>
-        <Text>Project</Text>
-      </TouchableOpacity>
+      {accessToken && (
+        <>
+          <View style={styles.header}>
+            <Text style={styles.headerH1}>Hello {email}, this is your projectboard</Text>
+          </View>
+          <TouchableOpacity onPress={() => navigation.navigate('SingleProject')}>
+            <Text>Project</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={logout}>
+            <Text>Sign out</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </ScrollView>
   )
 }
