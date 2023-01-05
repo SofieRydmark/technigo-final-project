8089b1464779ae737adaebc091128c9e23db2ca3
SofieRydmark <you@example.com>
2023-01-05 15:22:45 +0100

    update backend delete user

--- backend/endpoints/AdminAccount.js
+++ backend/endpoints/AdminAccount.js
@@ -1,64 +1,64 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
 const User = require('../models/User.js')
 
 const crypto = require('crypto')
 
 export const deleteUser = async (req, res) => {
-    const { userId } = req.params
-    try {
-      const user = await User.findOneAndDelete({ userId })
-      if (user) {
-        res.status(200).json({
-          success: true,
-          response: 'Account removed :(',
-        })
-      } else {
-        res.status(400).json({
-          success: false,
-          response: 'User not found',
-        })
-      }
-    } catch (error) {
-      res.status(500).json({
+  const { userId } = req.params
+  try {
+    const user = await User.find({ _id: userId })
+    if (user) {
+      const deleteUser = await User.findOneAndDelete({ _id: userId })
+      res.status(200).json({
+        success: true,
+        response: 'Account removed :(',
+        data: deleteUser,
+      })
+    } else {
+      res.status(400).json({
         success: false,
-        response: error,
+        response: 'User not found',
       })
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+    })
   }
-
+}
 
 export const changePassword = async (req, res) => {
-    const { userId } = req.params
-    const { password } = req.body
-    const salt = bcrypt.genSaltSync()
-    try {
-      const user = await User.findOne({ userId })
-      if (user) {
-        const newPassword = bcrypt.hashSync(password, salt)
-        const updateUser = await User.findByIdAndUpdate(
-          { _id: userId },
-          {
-            $set: {
-              password: newPassword,
-            },
-          }
-        )
-        res.status(200).json({
-          success: true,
-          data: updateUser,
-          response: 'Password changed',
-        })
-      } else {
-        res.status(400).json({
-          success: false,
-          response: 'User not found',
-        })
-      }
-    } catch (error) {
-      res.status(500).json({
+  const { userId } = req.params
+  const { password } = req.body
+  const salt = bcrypt.genSaltSync()
+  try {
+    const user = await User.findOne({ userId })
+    if (user) {
+      const newPassword = bcrypt.hashSync(password, salt)
+      const updateUser = await User.findByIdAndUpdate(
+        { _id: userId },
+        {
+          $set: {
+            password: newPassword,
+          },
+        }
+      )
+      res.status(200).json({
+        success: true,
+        data: updateUser,
+        response: 'Password changed',
+      })
+    } else {
+      res.status(400).json({
         success: false,
-        response: error,
+        response: 'User not found',
       })
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+    })
   }
-  
+}
